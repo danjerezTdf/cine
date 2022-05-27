828fdf06711c05b5ca7e3b59dd78e1853c5bdb63
danjerezTdf <104039810+danjerezTdf@users.noreply.github.com>
2022-05-27 15:43:36 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
+/*
+Cine
+•Diseñar un algoritmo que recorra las butacas de una sala de cine y determine cuántas butacas desocupadas hay
+•Suponga que para modelar este problema, se utiliza un arreglo con valores lógicos
+•La presencia de un valor verdadero (true) en el arreglo indica que la butaca está ocupada
+•La presencia de un valor falso (false) en el arreglo indica que la butaca está desocupada
+*/
+
 let bucatasEstado: boolean[] = [
   false,
   true,
